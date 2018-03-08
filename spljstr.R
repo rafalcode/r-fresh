e6434a348ac69df229f76bba1fb7f8c4734f0696
rf73 <Ramon Fallon>
2018-03-08 17:46:18 +0000

    some cha

--- spljstr.R
+++ spljstr.R
@@ -4,14 +4,15 @@ library(stringi)
 library(stringr)
 cw <- getwd()
 # str_match(cw, "(/([^/]+))+")
-cws<-str_split(cw, '/')
+# cws<-str_split(cw, '/')
 # nah, need the simplify
 # cws<-str_split(cw, '/', simplify=T)
 ## we could the core strsplit, but it makes the paste render a vector, not clear how though.
-# cws<-strsplit(cw, '/')
+cws<-strsplit(cw, '/')
 # cwsp<-paste(cws, sep='/', collapse='')
 # cwsp<-paste(cws[1:3], collapse='/')
-cwsp<-stri_join_list(cws[1:3], sep='/')
+cwsp<-paste(cws[1:3], sep='/')
+# cwsp<-stri_join_list(cws[1:3], sep='/')
 str(cws)
 cws
 str(cwsp)
