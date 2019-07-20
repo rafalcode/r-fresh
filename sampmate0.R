e6b3fc40f03c04970f2d6525d1461c36d1dd3bb0
rafalcode <ramonfallon@gmail.com>
2019-07-20 16:42:04 +0100

    wrote function to generate a new gt given parent's gts: this is always random

--- sampmate0.R
+++ sampmate0.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-# this script does what? checks arguments
+# first part is the main section. There are functions at the bottom
 args <- commandArgs(trailingOnly = TRUE)
 numargs <- length(args)
 enumargs <- 1 # expected numargs
@@ -51,7 +51,46 @@ colnames(ma) <- attrinames
 fshuf <- sample((N/2+1):N, N/2)
 # so we can cbind() to our matrix, right?
 # Well, no. We need to give the female rows the number of the male they matched with
+# so the reverse of rshuf .. actually this is a tiny bit tricky
+fshufr <- rep(0,4) # create empty vector
+fshufr[fshuf-4] <- 1:4 # this is the operation that achieves the reverse of the permutation
+ma <- cbind(ma, c(fshuf, fshufr))
+# actually 
 # actually this is easy, just subtract N/2 from fshuf:
-ma <- cbind(ma, c(fshuf,fshuf-N/2))
 attrinames <- c(attrinames, 'MTE')
 colnames(ma) <- attrinames
+
+# OK, so we've randomly paired up this generation. how will the 2nd generation pan out?
+# Let's make another matrix for them, let's decide on the size first one column less than ma
+# how many offspring will they have? We'll hardcode that in:
+ma2c <- ncol(ma)-1
+noff <- 2
+ma2 <- matrix(rep(0, ma2c*noff*N/2), ncol=ma2c)
+ma[ma[1:4,4],3]
+
+# gengt(): function to generate a new gt assignment given two gt's.
+gengt <- function(gt1, gt2)
+{
+    if((gt1 == 0) && (gt2 ==0)) { 
+        return(0) # two ref alleles? always homozygous for ref allele
+    } else if(((gt1 == 0) && (gt2 ==2)) || ((gt1 == 2) && (gt2 ==0))) {
+        return(1) # homozyg for ref and homozyg for alt? Then always heterozyg
+    } else if((gt1 == 2) && (gt2 ==2)) {
+        return(2) # two alt alleles? always homozygous for alt allele
+    } else if(((gt1 == 0) && (gt2 ==1)) || ((gt1 == 1) && (gt2 ==0))) {
+        return(round(runif(1))) # homozyg for ref and heterozyg? 50% chance of homozyg ref, 50% chance heterozyg
+    } else if(((gt1 == 2) && (gt2 ==1)) || ((gt1 == 1) && (gt2 ==2))) {
+        return(round(1+runif(1))) # homozyg for alt and heterozyg? 50% chance of homozyg alt, 50% chance heterozyg
+    } else if((gt1 == 1) && (gt2 ==1)) {
+        # both heterozyg's? slightly more complicated: 25% chance homozyg ref, 50% heterozyg, 25 homozyg alt
+        r <- runif(1)
+        if(r < 0.25) {
+            return(0)
+        } else if(r < 0.75) {
+            return(1)
+        } else {
+            return(2)
+        }
+    }
+}
+
