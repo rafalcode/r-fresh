db5d2e9207120d852aae54b8054b8a244d7fd956
Ramon Fallon <ramonfallon@gmail.com>
2022-06-14 22:51:19 +0100

    minor change or simple comment

--- gr1.R
+++ gr1.R
@@ -5,15 +5,21 @@
 library(tidyverse)
 library(Cairo)
 
-d <- data.frame(x=1:3, y = c(12, -9, 7))
+d <- data.frame(x=1:3, y = c(12, -39, 7))
 rownames(d) <- sprintf("%02i", as.integer(rownames(d)))
-lx <- dim(d)[1]
 minl <- min(d$y)
-barwidth <- 0.6
+maxl <- min(d$y)
+if(abs(minl) > maxl) {
+    ra <- abs(minl)
+} else {
+    ra <- maxl
+}
+
+barwidth <- 0.8
 bd2 <- barwidth/2
 
 # interpolate values from zero to y
-vals <- lapply(d$y, function(y) seq(minl, y, by = 0.05))
+vals <- lapply(d$y, function(y) seq(0, y, by = sign(y)*.025))
 y <- unlist(vals)
 # create corresponding number of x values
 mid <- rep(d$x, lengths(vals))
@@ -24,5 +30,11 @@ d2 <- data.frame(x = mid - bd2, xend = mid + bd2, y = y, yend = y)
 CairoPNG("fname.png", 800, 800)
 ggplot(data = d2, aes(x = x, xend = xend, y = y, yend = yend, color = y)) +
   geom_segment(size = 1) +
-  scale_color_gradient2(low = "red", mid = "white", high = "green", midpoint = 0)
+  # scale_color_gradient2(low = "red", mid = "white", high = "green", midpoint = 0) +
+  scale_color_gradient2(low = "midnightblue", mid = "grey", high = "green4", midpoint = 0) +
+  theme(legend.position="none", panel.background = element_blank(), 
+        axis.line.y=element_line(size = 0.5, linetype = "solid", colour = "black"),
+        panel.grid.major = element_blank()) +
+  ylim(-ra, ra) +
+  geom_hline(yintercept=0)
 dev.off()
