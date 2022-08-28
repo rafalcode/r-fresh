fc8994a322ba2bf81a7bbf84b7dacc2b2cfbc20a
Ramon Fallon <ramonfallon@gmail.com>
2022-08-28 22:30:05 +0100

    minor change or simple comment

---
+++ rb0.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+# Beginning RnBEads vignette perhaps?
+library(ggplot2)
+library(Cairo)
+library(RnBeads)
+library(RnBeads.hg19)
+
+# Directory where your data is located
+# data.dir <- "~/RnBeads/data/Ziller2011_PLoSGen_450K"
+# idat.dir <- file.path(data.dir, "idat")
+# sample.annotation <- file.path(data.dir, "sample_annotation.csv")
+# Directory where the output should be written to
+# analysis.dir <- "~/RnBeads/analysis"
+# Directory where the report files should be written to
+# report.dir <- file.path(analysis.dir, "reports")
+
+# rnb.options(filtering.sex.chromosomes.removal=TRUE, identifiers.column="Sample_ID")
+
+data(small.example.object)
+# rnb.set.example
+
+
+
+
+
+
+
+
+
+
+
+
+# Cairo image template
+# CairoPNG("fname.png", 800, 800)
+# put plot command here
+# dev.off()
