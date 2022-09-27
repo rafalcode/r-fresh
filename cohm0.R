0fa02b75c36fda628aa6c50a7a6aa288e865b09a
nutria <nutria@p79t.rcsi-internal.ie>
2022-09-27 15:48:57 +0100

    minor change or simple comment

---
+++ cohm0.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+# ComplexHeatmap tests, the anno_lines function.
+# these are long thin recngular box to add onto heatmaps.
+library(ComplexHeatmap)
+library(Cairo)
+
+anno = anno_lines(runif(10))
+CairoPNG("cohm0.png", 800, 800)
+draw(anno, test = "anno_lines")
+dev.off()
+
+anno = anno_lines(cbind(c(1:5, 1:5), c(5:1, 5:1)), gp = gpar(col = 2:3))
+CairoPNG("cohm1.png", 800, 800)
+draw(anno, test = "matrix")
+dev.off()
+
+anno = anno_lines(cbind(c(1:5, 1:5), c(5:1, 5:1)), gp = gpar(col = 2:3),
+add_points = TRUE, pt_gp = gpar(col = 5:6), pch = c(1, 16))
+CairoPNG("cohm1.png", 800, 800)
+draw(anno, test = "matrix")
+dev.off()
