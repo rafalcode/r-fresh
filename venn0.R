e4a7ea0719d0d995f04c38abe2856dca75de5419
Ramon Fallon <ramonfallon@gmail.com>
2023-04-24 15:56:37 +0100

    minor change or simple comment

---
+++ venn0.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+# testin gout VennDetail.
+library(VennDetail)
+library(Cairo)
+
+# Cairo image template
+# CairoPNG("fname.png", 800, 800)
+# put plot command here
+# dev.off()
