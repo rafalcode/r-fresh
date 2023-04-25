eeb8dbeb772fc1752fd66e63798328e6f78d61b5
Ramon Fallon <ramonfallon@gmail.com>
2023-04-25 23:15:39 +0100

    minor change or simple comment

--- venn0.R
+++ venn0.R
@@ -1,15 +1,32 @@
 #!/usr/bin/env Rscript
 # testing out VennDetail.
-library(VennDetail)
 library(Cairo)
 library(VennDetail)
 
 # Hinder et al paper
 data(T2DM)
 
-ven <- venndetail(list(Cortex = T2DM$Cortex$Entrez, SCN = T2DM$SCN$Entrez, Glom = T2DM$Glom$Entrez))
+# list to vennize
+l2v <- list(Cortex = T2DM$Cortex$Entrez, SCN = T2DM$SCN$Entrez, Glom = T2DM$Glom$Entrez)
+ven <- venndetail(l2v)
 
 # Cairo image template
 CairoPNG("venn0.png", 800, 800)
+plot(ven)
+dev.off()
+
+# random two letter words:
+ii <- 1+round(3*runif(64))
+ss <- seq(1,64,2)
+ss2 <- seq(2,64,2)
+le <- paste0(letters[ii[ss]], letters[ii[ss2]])
+l1 <- le[1:16]
+l2 <- le[17:32]
+
+l2v <- list(Cortex = l1, SCN = l2)
+ven <- venndetail(l2v)
+
+# Cairo image template
+CairoPNG("venn1.png", 800, 800)
 plot(ven)
 dev.off()
