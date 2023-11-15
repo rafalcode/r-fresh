fc875cbdede91bb786a312b274d9eeaeefdf1953
Ramon Fallon <ramonfallon@gmail.com>
2023-11-15 16:21:58 +0000

    minor change or simple comment

--- reactome.R
+++ reactome.R
@@ -9,6 +9,7 @@ data(geneList, package="DOSE")
 
 de <- names(geneList)[abs(geneList) > 1.5] # de, diff exp I expect.
 ## [1] "4312"  "8318"  "10874" "55143" "55388" "991"
+# i.e. ENTREZIDs!
 
 x <- enrichPathway(gene=de, pvalueCutoff = 0.05, readable=TRUE)
 
@@ -25,9 +26,14 @@ CairoPNG("reactomeerdp.png", 800, 800)
 dp <- dotplot(x, showCategory=30) + ggtitle("dotplot for Reactome")
 show(dp)
 dev.off()
+
 CairoPNG("reactomegrdp.png", 800, 800)
 # y is gseaResult so this may work ...
 dp <- dotplot(y, showCategory=30) + ggtitle("dotplot for Reactome")
 # only 1 pvalue? Odd. not very satisfying.
 show(dp)
 dev.off()
+
+# conclusions
+# dotplot on the enrichResults is much better
+# reactome needs entrezids!
