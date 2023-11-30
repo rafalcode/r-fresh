da9b6e2ac481bcfbb5836cbcbf803c877e1a320e
Ramon Fallon <ramonfallon@gmail.com>
2023-11-30 13:00:31 +0000

    minor change or simple comment

---
+++ pfi.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# this script does what?
+library(ggplot2)
+library(Cairo)
+library(pathfindR)
+
+# Cairo image template
+# CairoPNG("fname.png", 800, 800)
+# put plot command here
+# dev.off()
+
+# example_pathfindR_input
+# available immediately?
+
+# Gene.symbol	logFC	adj.P.Val
+# FAM110A	-0.6939359	0.0000034
+# RNASE2	1.3535040	0.0000101
+# S100A8	1.5448338	0.0000347
+# S100A9	1.0280904	0.0002263
+# TEX261	-0.3235994	0.0002263
+# ARHGAP17	-0.6919330	0.0002708
+
+# By default, run_pathfindR() creates a temporary directory for writing the output files, including active subnetwork search results and a HTML report. To set the output directory, use output_dir:
+output_df <- run_pathfindR(example_pathfindR_input, p_val_threshold = 0.01, output_dir = "pfidir")
