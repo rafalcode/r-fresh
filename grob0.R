1b8acf0afab38e9338ac1107071eca774a823b83
nutria <nutria@p79t.rcsi-internal.ie>
2023-12-01 17:22:32 +0000

    minor change or simple comment

---
+++ grob0.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+# I'm trying to understand this grob thing and how to apply it
+library(Cairo)
+library(grid)
+
+CairoPNG("grob0.png", 800, 800)
+# grid.rect(gp=gpar(col="red"))
+# same thing, but more verbose
+grid.draw(applyEdit(rectGrob(), gEdit(gp=gpar(col="red"))))
+# Cairo image template
+# put plot command here
+dev.off()
+
+# Learnings:
+# Yes, 
+# grid.rect(gp=gpar(col="red"))
+# and
+# grid.draw(applyEdit(rectGrob(), gEdit(gp=gpar(col="red"))))
+# do prodcue exactly the same thing.
+# it does produce and image of white fill
+# about 600 or 800 square (a default at work?)
+# you can faintly detect red on the outside
+
