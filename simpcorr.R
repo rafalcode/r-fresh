f8dc4aebffb2cff0d8a340fe1a8ef0c10e40109c
Ramon Fallon <ramonfallon@gmail.com>
2024-02-26 22:45:45 +0000

    minor change or simple comment

--- simpcorr.R
+++ simpcorr.R
@@ -11,10 +11,9 @@ library(Cairo)
 
 
 # this is your typical correlation sinulation. Firs we start with the predictor variable x1
-N   <- 10000
-rho <- 0.93
-# x1  <- rnorm(n = N, mean = 0, sd = 1)
-x1  <- rnorm(N, 0.5, 0.3) # this is the same, as above are defaults.
+rho <- 0.83
+x1  <- rnorm(500, mean = 0, sd = 1)
+# x1  <- 1:10000
 
 # OK the depedent variable, this is a stdanrd way to do it.
 # x2  <- (rho * x1) + sqrt(1 - rho*rho) * rnorm(n = N, mean = 0, sd = 1);
@@ -22,22 +21,17 @@ x1  <- rnorm(N, 0.5, 0.3) # this is the same, as above are defaults.
 rho2 <- rho*rho # come sto .09 in this case
 sq1mr <- sqrt(1-rho2) # square root of 1 minus rho squared, comes to .95 in this case
 # x2  <- (rho * x1) + sqrt(1 - rho*rho) * rnorm(N)
-x2  <- (rho * x1) + sq1mr*rnorm(N, .5, .3)
+x2  <- (rho * x1) + sq1mr*rnorm(length(x1), .5, .3)
 
-# what you get here is an x1 of .3 suprise surprise with incredibly low p-value.
-# of course you could say, of course, the normal distri "plays into its hands"
-# HOWEVER, note how small the rsquared is!
-# you can raise it, by bringing rho up to .8, this will give you an rqs of .635 (not that adjusted is the exact same!) 
+# despite this cofident pre-setting of correlation, it's only being pertrubed by normally distributed errors.
 
-# Cairo image template
-# CairoPNG("fname.png", 800, 800)
-# put plot command here
-# dev.off()
+
 Corr<-cor(x2, x1, method = "spearman")
-CairoPNG("smrapssca.png", 800, 800)
+CairoPNG("smpcorr0.png", 800, 800)
 smoothScatter(x1, x2, transformation = function(x) x ^ 0.4,
                 colramp = colorRampPalette(c("#000099", "#00FEFF", "#45FE4F",
                                              "#FCFF00", "#FF9400", "#FF3100")),
+              bandwidth=5,
                 xlab = "x1",
                 ylab = "x2", xlim=0:1, ylim=0:1,
                 font.lab=2, cex.lab=1.4, cex.axis=1.2, cex.main=1.5, main="Sim. of Correlation")
@@ -47,3 +41,11 @@ smoothScatter(x1, x2, transformation = function(x) x ^ 0.4,
   abline(a=0, b=1)
   text(bquote(r^2 == .(format(Corr, digits=5))), x=0.2,y=0.9, col = "white", cex = 3)
   dev.off()
+df <- data.frame(x1=x1, x2=x2)
+CairoPNG("smpcorr1.png", 800, 800)
+gp <- ggplot(df, aes(x=x1, y=x2)) +
+    geom_point(alpha=.2, size=3)
+show(gp)
+dev.off()
+
+# scatter plots colorimetrics: I already hit up on it, they end up being symmetrical
